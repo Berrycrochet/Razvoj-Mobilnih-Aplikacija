22e64ca196ed32acb0b03368128bdba3446a1b52
Berrina <125799240+Berrycrochet@users.noreply.github.com>
2026-01-20 14:24:31 +0100

    SASTAVLJENA VERZIJA koja radi
    ispravljena verzija koja sva radi napokon

---
+++ Side Hustle Manager/Side Hustle Manager/Pages/User/UserProfilePage.xaml.cs
@@ -0,0 +1,81 @@
+using Microsoft.Maui.Devices.Sensors; // za Geolocation
+using Microsoft.Maui.ApplicationModel; // za permissions
+using Microsoft.Maui.Essentials;
+
+
+namespace Side_Hustle_Manager.Pages.User 
+{
+
+    public partial class UserProfilePage : ContentPage
+    {
+        public UserProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        // Logout dugme
+
+        private async void OnLogoutClicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlertAsync("Logout", "Are you sure you want to logout?", "Yes", "No");
+            if (answer)
+            {
+                Application.Current.MainPage = new NavigationPage(new LoginPage());
+            }
+        }
+
+
+
+
+        private async void OnGetLocationClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                var location = await Geolocation.Default.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    // Reverse geocoding
+                    var placemarks = await Geocoding.Default.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = placemarks?.FirstOrDefault();
+
+                    if (placemark != null)
+                    {
+                        string city = placemark.Locality; // ovo je grad
+                        string country = placemark.CountryName;
+                        LocationLabel.Text = $"City: {city}, Country: {country}";
+                    }
+                    else
+                    {
+                        LocationLabel.Text = "City not found";
+                    }
+                }
+                else
+                {
+                    LocationLabel.Text = "Unable to get location";
+                }
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlertAsync("Error", "Feature not supported on this device", "OK");
+            }
+            catch (FeatureNotEnabledException)
+            {
+                await DisplayAlertAsync("Error", "Please enable location services", "OK");
+            }
+            catch (PermissionException)
+            {
+                await DisplayAlertAsync("Error", "Location permission denied", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Error", $"Something went wrong: {ex.Message}", "OK");
+            }
+        }
+
+
+
+    }
+}
+
